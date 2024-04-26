27739e933486ee379122009ccdf7857dcdf2ae3b
joec11 <jc625@njit.edu>
2024-04-26 18:11:42 -0400

    Modified app/routers/user_routes.py by adding the linkedin_profile_url, the github_profile_url, and the is_professional fields to the UserResponse model_construct that is being returned by the create_user function.

--- app/routers/user_routes.py
+++ app/routers/user_routes.py
@@ -161,7 +161,10 @@ async def create_user(user: UserCreate, request: Request, db: AsyncSession = Dep
         last_login_at=created_user.last_login_at,
         created_at=created_user.created_at,
         updated_at=created_user.updated_at,
-        links=create_user_links(created_user.id, request)
+        links=create_user_links(created_user.id, request),
+        linkedin_profile_url=created_user.linkedin_profile_url,
+        github_profile_url=created_user.github_profile_url,
+        is_professional=created_user.is_professional
     )
 
 
